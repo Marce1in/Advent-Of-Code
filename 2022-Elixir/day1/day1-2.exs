e04e61bc492c58c866717dc6a5ce4a18cadc631f
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2026-03-11 19:11:43 -0300

    feat: day1-2 solution elixir 2022

---
+++ 2022-Elixir/day1/day1-2.exs
@@ -0,0 +1,20 @@
+{:ok, values} = File.read("input")
+
+values
+|> IO.inspect(label: "Valor seco")
+|> String.split("\n\n", trim: true)
+|> IO.inspect(label: "Valores splitado por linhas")
+|> Enum.map(&String.split(&1, "\n", trim: true))
+|> IO.inspect(label: "Todos valores splitado")
+|> Enum.map(fn row ->
+  Enum.map(row, &String.to_integer/1)
+  |> IO.inspect(label: "Valores em int")
+  |> Enum.sum()
+end)
+|> IO.inspect(label: "Valores em somados")
+|> Enum.sort(:desc)
+|> IO.inspect(label: "Valores ordenados")
+|> Enum.take(3)
+|> IO.inspect(label: "Top 3 valores")
+|> Enum.sum()
+|> IO.inspect(label: "Top 3 Valores somados")
